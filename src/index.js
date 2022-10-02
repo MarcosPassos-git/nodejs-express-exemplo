b6c6b231af6346d91680b1df69bec94c888a47c1
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-10-01 21:41:59 -0300

    Criado controle para user e post

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-const express = require('express')
+import * as express from 'express'
 const app = express()
 
+import userController from './controller/userController'
+import postController from './controller/postController'
+
+app.use('/user', userController)
+app.use('/post', postController)
+
+
+/*
+
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('GET /')
 })
+
+app.post('/', (req, res) => {
+    res.status(201).send('POST /')
+})
+
+app.put('/', (req, res) => {
+    res.send('PUT /')
+})
+
+app.delete('/', (req, res) => {
+    res.send('DELETE /')
+})
+
+app.patch('/', (req, res) => {
+    res.send('PATCH /')
+})
+*/
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
